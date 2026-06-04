9a3775ccce09a90e66840118605ba1a3e0d00c7c
MrWoxi <klimovichvita2005@gmail.com>
2026-06-04 12:53:02 +0300

    Add more tests and result files

--- BugTests/Test1.cs
+++ BugTests/Test1.cs
@@ -20,7 +20,7 @@ namespace BugTests
         }
 
         [TestMethod]
-        public void Open_Assign_GoesToAssigned()
+        public void Open_Assign()
         {
             var bug = new Bug();
             bug.Assign();
@@ -28,7 +28,7 @@ namespace BugTests
         }
 
         [TestMethod]
-        public void Assigned_StartFix_GoesToInProgress()
+        public void Assigned_StartFix()
         {
             var bug = new Bug(Bug.State.Assigned);
             bug.StartFix();
@@ -36,7 +36,7 @@ namespace BugTests
         }
 
         [TestMethod]
-        public void InProgress_Resolve_GoesToResolved()
+        public void InProgress_Resolve()
         {
             var bug = new Bug(Bug.State.InProgress);
             bug.Resolve();
@@ -44,7 +44,7 @@ namespace BugTests
         }
 
         [TestMethod]
-        public void Resolved_Verify_GoesToClosed()
+        public void Resolved_Verify()
         {
             var bug = new Bug(Bug.State.Resolved);
             bug.Verify();
@@ -63,11 +63,17 @@ namespace BugTests
         }
 
         [TestMethod]
-        public void Assigned_Defer_Resume()
+        public void Assigned_Defer()
         {
             var bug = new Bug(Bug.State.Assigned);
             bug.Defer();
             Assert.AreEqual(Bug.State.Deferred, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Deferred_Resume()
+        {
+            var bug = new Bug(Bug.State.Deferred);
             bug.Resume();
             Assert.AreEqual(Bug.State.Assigned, bug.CurrentState);
         }
@@ -89,6 +95,14 @@ namespace BugTests
         }
 
         [TestMethod]
+        public void Rejected_Verify()
+        {
+            var bug = new Bug(Bug.State.Rejected);
+            bug.Verify();
+            Assert.AreEqual(Bug.State.Closed, bug.CurrentState);
+        }
+
+        [TestMethod]
         public void Assigned_CannotReproduce()
         {
             var bug = new Bug(Bug.State.Assigned);
@@ -97,29 +111,47 @@ namespace BugTests
         }
 
         [TestMethod]
-        public void NotReproducible_Confirm_Deny()
+        public void NotReproducible_Confirm()
         {
-            var bug1 = new Bug(Bug.State.NotReproducible);
-            bug1.Confirm();
-            Assert.AreEqual(Bug.State.Closed, bug1.CurrentState);
-
-            var bug2 = new Bug(Bug.State.NotReproducible);
-            bug2.Deny();
-            Assert.AreEqual(Bug.State.Assigned, bug2.CurrentState);
+            var bug = new Bug(Bug.State.NotReproducible);
+            bug.Confirm();
+            Assert.AreEqual(Bug.State.Closed, bug.CurrentState);
         }
 
         [TestMethod]
-        public void Closed_Reopen_ReturnToTriage()
+        public void NotReproducible_Deny()
+        {
+            var bug = new Bug(Bug.State.NotReproducible);
+            bug.Deny();
+            Assert.AreEqual(Bug.State.Assigned, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Resolved_Reopen()
+        {
+            var bug = new Bug(Bug.State.Resolved);
+            bug.Reopen();
+            Assert.AreEqual(Bug.State.Reopened, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Closed_Reopen()
         {
             var bug = new Bug(Bug.State.Closed);
             bug.Reopen();
             Assert.AreEqual(Bug.State.Reopened, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Reopened_ReturnToTriage()
+        {
+            var bug = new Bug(Bug.State.Reopened);
             bug.ReturnToTriage();
             Assert.AreEqual(Bug.State.Assigned, bug.CurrentState);
         }
 
         [TestMethod]
-        public void ReopenedCanBeClosedAgain()
+        public void ReopenedBugCanClose()
         {
             var bug = new Bug(Bug.State.Closed);
             bug.Reopen();
@@ -131,15 +163,16 @@ namespace BugTests
         }
 
         [TestMethod]
-        public void CanFire_OpenAllowsAssign()
+        public void CanFire_OpenOnlyAssign()
         {
             var bug = new Bug();
             Assert.IsTrue(bug.CanFire(Bug.Trigger.Assign));
             Assert.IsFalse(bug.CanFire(Bug.Trigger.Resolve));
+            Assert.IsFalse(bug.CanFire(Bug.Trigger.StartFix));
         }
 
         [TestMethod]
-        public void CanFire_ClosedAllowsReopen()
+        public void CanFire_ClosedOnlyReopen()
         {
             var bug = new Bug(Bug.State.Closed);
             Assert.IsTrue(bug.CanFire(Bug.Trigger.Reopen));
@@ -147,19 +180,72 @@ namespace BugTests
         }
 
         [TestMethod]
-        public void InvalidTransitions_Throw()
+        public void CanFire_AssignedFourOptions()
+        {
+            var bug = new Bug(Bug.State.Assigned);
+            Assert.IsTrue(bug.CanFire(Bug.Trigger.StartFix));
+            Assert.IsTrue(bug.CanFire(Bug.Trigger.Defer));
+            Assert.IsTrue(bug.CanFire(Bug.Trigger.Reject));
+            Assert.IsTrue(bug.CanFire(Bug.Trigger.CannotReproduce));
+        }
+
+        [TestMethod]
+        public void Open_Resolve_Throws()
         {
             var bug = new Bug();
             Assert.ThrowsExactly<InvalidOperationException>(() => bug.Resolve());
+        }
+
+        [TestMethod]
+        public void Open_StartFix_Throws()
+        {
+            var bug = new Bug();
             Assert.ThrowsExactly<InvalidOperationException>(() => bug.StartFix());
         }
 
         [TestMethod]
-        public void InvalidTransition_MessageMentionsState()
+        public void Closed_Verify_Throws()
+        {
+            var bug = new Bug(Bug.State.Closed);
+            Assert.ThrowsExactly<InvalidOperationException>(() => bug.Verify());
+        }
+
+        [TestMethod]
+        public void InProgress_Verify_Throws()
+        {
+            var bug = new Bug(Bug.State.InProgress);
+            Assert.ThrowsExactly<InvalidOperationException>(() => bug.Verify());
+        }
+
+        [TestMethod]
+        public void Deferred_StartFix_Throws()
+        {
+            var bug = new Bug(Bug.State.Deferred);
+            Assert.ThrowsExactly<InvalidOperationException>(() => bug.StartFix());
+        }
+
+        [TestMethod]
+        public void ErrorMessageContainsState()
         {
             var bug = new Bug();
             var ex = Assert.ThrowsExactly<InvalidOperationException>(() => bug.Resolve());
             StringAssert.Contains(ex.Message, "Open");
+        }
+
+        [TestMethod]
+        public void Fire_ValidTrigger()
+        {
+            var bug = new Bug();
+            bug.Fire(Bug.Trigger.Assign);
+            Assert.AreEqual(Bug.State.Assigned, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Fire_InvalidTrigger_Throws()
+        {
+            var bug = new Bug();
+            Assert.ThrowsExactly<InvalidOperationException>(
+                () => bug.Fire(Bug.Trigger.Resolve));
         }
     }
 }
